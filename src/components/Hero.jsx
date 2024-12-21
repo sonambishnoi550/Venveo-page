bc0bfe38bf7ea72816c0773bd1e2c6badb1bb8db
sonambishnoi550 <bishnoisonam079@gmail.com>
2024-12-21 12:43:23 +0530

    same

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,14 +8,14 @@ const Hero = () => {
             <div className="container">
                 <h1 className='pt-[79px] max-sm:pt-[43px] text-custom-9xl xl:leading-[104px] max-xl:text-custom-8xl !font-bold leading-[97%] max-md:text-5xl uppercase sohne text-white max-w-[822px] mx-auto text-center'>Make marketing
                     <span className='block'></span>     a growth machine</h1>
-                <img className='ml-[273px] max-xl:ml-[200px] max-sm:ml-10 pointer-events-none max-sm:max-w-[146px]' src="./assets/images/svg/green-line.svg" alt="line" />
+                <img className='ml-[273px] max-xl:ml-[200px] max-sm:ml-10 pointer-events-none max-sm:max-w-[146px] mt-1' src="./assets/images/svg/green-line.svg" alt="line" />
                 <p className='pt-3 max-md:pt-1 text-white  max-w-[656px] mx-auto text-center max-sm:text-custom-2xl text-2xl leading-custom-lg'>We focus on results. Not just chatter. Elevate your business with our top-tier services and tech. Get a proposal now.</p>
                 <form  className="border-2 border-white mt-[30px] text-white rounded-[50px] text-center py-4 sm:py-5 px-7 flex max-w-[448px] sm:mx-auto mx-5 justify-between">
                     <input
                         id="email"
                         type="text"
                         placeholder="Enter Your Website"
-                        className="bg-transparent outline-none flex-grow placeholder:text-light-gray text-light-gray text-custom-xs font-normal leading-5"
+                        className="bg-transparent outline-none  placeholder:text-gray-400  text-custom-xs font-normal leading-5"
                         aria-label="Enter your website"
                         required
                     />
