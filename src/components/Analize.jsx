b6c7aa3771e131eda68e88bb02ba9891158f6fff
sonambishnoi550 <bishnoisonam079@gmail.com>
2025-01-02 13:50:40 +0530

    adjust line

--- src/components/Analize.jsx
+++ src/components/Analize.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 const Analize = () => {
     return (
         <div style={{ backgroundImage: `url(./assets/images/webp/Analyze-bg.webp)` }} className="mt-20 max-xl:mt-12 max-sm:mt-8 bg-cover bg-no-repeat bg-center pt-16 pb-20 xl:px-32 max-sm:pb-11 max-sm:pt-8 relative">
-            <img
-                src="/assets/images/svg/analize.svg"
-                alt="img"
-                className="absolute 2xl:left-[180rem] xl:left-[43rem] lg:left-[28rem] max-lg:hidden xl:top-[6rem] lg:top-[8rem] top-[11rem] sm:left-[30rem] left-[4rem] sm:top-[8rem] pointer-events-none"
-            />
-            <div className="container">
+           
+            <div className="container relative">
+                <img
+                    src="/assets/images/svg/analize.svg"
+                    alt="img"
+                    className="absolute xl:left-[33rem] xl:top-[3rem]  lg:left-[31rem] max-lg:hidden lg:top-[4rem] top-[11rem] sm:left-[30rem] left-[4rem] sm:top-[8rem] pointer-events-none"
+                />
                 <div className="md:flex justify-between max-lg:flex-col">
                     <div className="flex-col">
                         <p className="text-custom-xs font-normal leading-6 text-left pb-2 font-space max-lg:text-center">
