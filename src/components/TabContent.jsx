e18756c2843fb06460897e0303579df7cb41eb72
sonambishnoi550 <bishnoisonam079@gmail.com>
2025-01-02 11:13:14 +0530

    change favicon

--- src/components/TabContent.jsx
+++ src/components/TabContent.jsx
@@ -13,7 +13,7 @@ const Tab = ({ activeTab }) => {
                             <p className="font-normal text-custom-xs text-left text-white pb-[98px] max-lg:pb-[57px] max-sm:pb-[14px] max-sm:text-custom-xs font-maisonMono">
                                 This is placeholder but can add more to the business of building, some epic fails (and how to avoid them), and everything building science.
                             </p>
-                            <div className="flex max-lg:pb-20 max-sm:pb-6 max-sm:w-full">
+                            <div className="flex max-lg:pb-10 max-sm:pb-6 max-sm:w-full">
                                 <a href="#" className="bg-red-500 flex px-[32px] py-[31px] text-white text-custom-xs pointer-events-none max-sm:w-full max-sm:pl-[57px]"><img className="mr-[10px]" src="./assets/images/svg/subscribe.svg" alt="" />
                                     Subscribe to Podcast <span className="text-custom-xxs ml-1 mt-[10px]"> ▼</span>
                                 </a>
@@ -51,7 +51,7 @@ const Tab = ({ activeTab }) => {
             );
         case "tab3":
             return (
-                <div style={{ backgroundImage: `url(./assets/images/webp/tab-three-bg.webp)` }} className="max-w-[1280px] px-[79px] pt-[75px] max-lg:pt-[45px] h-[720px] max-xl:h-[620px] max-lg:h-[720px] max-md:px-[32px]">
+                <div style={{ backgroundImage: `url(./assets/images/webp/tab-three-bg.webp)` }} className="max-w-[1280px] px-[79px] pt-[75px] max-lg:pt-[45px] h-[720px] max-xl:h-[620px] max-lg:h-full max-md:px-[32px]">
                     <div className="lg:flex justify-between">
                         <div className="lg:w-5/12">
                             <h2 className="text-custom-7xl max-md:text-5xl leading-[70px] font-sohne font-bold max-xl:text-6xl text-left pb-2 text-white uppercase">
@@ -61,12 +61,12 @@ const Tab = ({ activeTab }) => {
                                 Lorem ipsum what this product actually does. </p>
                             <p className="text-white text-custom-xs font-normal leading-5 max-w-[360px] max-xl:max-w-none font-maisonMono">This is placeholder but can add more the business of building, some epic fails (and how to avoid them) and everything building science.</p>
                             <div className="flex mt-16 max-sm:mt-7 max-lg:pb-14">
-                                <a href="#" className="bg-yellow text-custom-xs font-semibold text-center py-8 px-8 flex gap-[10px] pointer-events-none">
+                                <a href="#" className="bg-yellow text-custom-xs font-semibold text-center py-8 px-8 max-sm:p-4 flex gap-[10px] pointer-events-none">
                                     Call to Action Somewhere<img src="./assets/images/svg/call-arrow.svg" alt="arrow" /></a>
                             </div>
                         </div>
                         <div className="lg:w-5/12 w-full">
-                            <img className="w-full pointer-events-none" src="./assets/images/webp/tab-three-card.webp" alt="Tab Three Map" />
+                            <img className="w-full pointer-events-none pb-8" src="./assets/images/webp/tab-three-card.webp" alt="Tab Three Map" />
                         </div>
                     </div>
                 </div>
@@ -80,7 +80,7 @@ const TabsComponent = () => {
     const [activeTab, setActiveTab] = useState("tab1");
 
     return (
-        <section className="py-14 max-sm:pt-[64px] flex justify-center">
+        <section className="py-14 max-sm:pt-[64px] max-sm:pb-6 flex justify-center">
             <div className="container max-w-[1280px] mx-auto">
                 <h2 className="text-custom-7xl font-bold text-center uppercase font-sohne leading-[70px] max-sm:text-6xl">The Growth Partner</h2>
                 <p className="text-3xl font-light text-center pb-[43px] max-sm:pb-[19px] max-sm:text-2xl font-maisonLight">you’ve been looking for.</p>
